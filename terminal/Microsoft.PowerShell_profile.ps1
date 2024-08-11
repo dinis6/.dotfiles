ed8282b2068e41fc76964ad96108aa0eb485c22a
QXZ25RY <Dinis.Ferreira@ctw.bmwgroup.com>
2024-08-11 13:17:08 +0100

    Initial commit

---
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+Import-Module -Name Terminal-Icons
+oh-my-posh init pwsh --config "C:\Users\ctw01660\Documents\PowerShell\amro.omp.json" | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
